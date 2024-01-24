a6cb711238803829f283b20deed58e514de1cfde
Juliane Cassidy <julianecassidy@gmail.com>
2024-01-23 19:52:04 -0800

    add config, db, and dependencies

---
+++ config.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/** Common config for RandomRhythms API */
+
+// read .env files and make environmental variables
+
+require("dotenv").config();
+
+const DB_URI = (process.env.NODE_ENV === "test")
+    ? "postgresql:///randomrhythms"
+    : "postgresql:///randomrhythms_test";
+
+const SECRET_KEY = process.env.SECRET_KEY || "secret";
+
+const BCRYPT_WORK_FACTOR = 12;
+
+
+module.exports = {
+  DB_URI,
+  SECRET_KEY,
+  BCRYPT_WORK_FACTOR,
+};
---
+++ db.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/** Database setup for RandomRythms API. */
+
+const { Client } = require("pg");
+const { DB_URI } = require("./config");
+
+const db = new Client(DB_URI);
+
+db.connect();
+
+module.exports = db;
